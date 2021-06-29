10790ae38c3a21045c8a3a6debebfbf68f759739
Yevgeniy Musiayaka <yevgeniy.musiyaka@mail.ru>
2021-06-30 07:30:41 +1100

    Первый коммит

---
+++ javascript.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var arr = [];
+
+// let answer = confirm('Вы здесь?');
+
+// console.log( typeof(arr) );
+
+// let answer = +prompt('Ваш возаст', '18');
+
+// console.log( typeof(answer) );
+
+// console.log(4 + + "строка");
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+console.log(incr);
+console.log(decr);
